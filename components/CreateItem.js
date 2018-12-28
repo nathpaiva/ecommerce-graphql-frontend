789fb86f4faba48584244fe11ec1c0bbf9401dca
Nath Paiva <hello@nathpaiva.com.br>
2018-12-27 22:58:26 -0200

    Create a handlerChange

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -14,20 +14,40 @@ export default class CreateItem extends Component {
       largeImage: '',
       price: 0
     };
+
+    this.handlerChange = this.handlerChange.bind(this);
+  }
+
+  handlerChange(e) {
+    const { name, type, value } = e.target;
+    const val = type === 'number' ? parseFloat(value) : value;
+
+    this.setState({
+      [name]: val
+    });
   }
 
   render() {
     return (
-      <Form>
+      <Form onSubmit={(e) => {
+        e.preventDefault();
+        console.log(this.state);
+      }} >
         <fieldset>
           <label htmlFor="title">
             Title
-            <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title} />
+            <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title} onChange={this.handlerChange} />
+          </label>
+          <label htmlFor="price">
+            Price
+            <input type="number" id="price" name="price" placeholder="Price" required value={this.state.price} onChange={this.handlerChange} />
           </label>
           <label htmlFor="description">
             Description
-            <input type="text" id="description" name="description" placeholder="Description" required value={this.state.description} />
+            <textarea id="description" name="description" placeholder="Enter a description" required value={this.state.description} onChange={this.handlerChange} />
           </label>
+
+          <button type="submit">Submit</button>
         </fieldset>
         <h1>Sell an Item.</h1>
       </Form>
